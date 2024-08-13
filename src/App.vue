71c94a300390647f3adf736a7c2e9d2a5fcc6cad
deyuanzou <3115729294@qq.com>
2024-08-13 15:41:31 +0800

    引入vue-router

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,18 @@
     <el-button type="danger">Danger</el-button>
   </div> -->
 
-  <button class="btn">
+  <!-- <button class="btn">
     按钮
-  </button>
+  </button> -->
+  <router-view></router-view>
 
 
 
 </template>
 
 <style scoped>
-.btn {
+/* .btn {
   @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded 
   my-4 mx-4 hover:(bg-purple-900) focus:(ring-2);
-
-  
-}
+} */
 </style>
