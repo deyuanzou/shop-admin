7b3c9784bf341924ee50c32733fb34b974b9a380
deyuanzou <3115729294@qq.com>
2024-08-13 14:39:45 +0800

    完成项目依赖下载及引入

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div> -->
+
+  <button class="bg-purple-500 text-indigo-50 px-4 py-2">按钮</button>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+
+import 'virtual:windi.css'
+
+
+app.mount('#app')
