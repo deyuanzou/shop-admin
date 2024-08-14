6dfefb275b65af8f2c89da63ab8d0c8b3d7131a3
deyuanzou <3115729294@qq.com>
2024-08-14 14:21:19 +0800

    完成登录页的开发

---
+++ src/pages/login.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+   username:"",
+   password:""
+})
+
+const onSubmit = () => {
+    console.log('submit!')
+}
+</script>
+
+<template>
+    <el-row class="bg-indigo-500 min-h-screen">
+        <el-col :span="16" class="flex items-center justify-center">
+            <div>
+                <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
+                <div class="text-gray-200 text-sm">此站点是Vue3+Vite开发的商城后台</div>
+            </div>
+        </el-col>
+        <el-col :span="8" class="bg-light-50 flex items-center justify-center flex-col">
+                <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
+                <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
+                    <span class="h-1px w-16 bg-gray-200"></span>
+                    <span>账号密码登录</span>
+                    <span class="h-1px w-16 bg-gray-200"></span>
+                </div>
+                <el-form :model="form" class="w-250px">
+                    <el-form-item>
+                        <el-input v-model="form.username" placeholder="请输入用户名"/>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input v-model="form.password" placeholder="请输入密码"/>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button round type="primary" class="w-250px" color="#626aef">登 录</el-button>
+                    </el-form-item>
+                </el-form>
+        </el-col>
+    </el-row>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHashHistory,createWebHistory } from "vue-router";
 
 import Index from '~/pages/index.vue';
-import About from '~/pages/about.vue';
 import NotFound from '~/pages/404.vue';
+import Login from '~/pages/login.vue';
 
 const routes = [
     {
@@ -10,13 +10,13 @@ const routes = [
         component: Index,
     },
     {
-        path: "/about",
-        component: About,
-    },
-    {
         path: '/:pathMatch(.*)*',
         name: 'NotFound',
         component: NotFound
+    },
+    {
+        path: '/login',
+        component: Login
     }
 ]
 
