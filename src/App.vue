32d13d10442173095b1233cae8d7b0b6814d873b
deyuanzou <3115729294@qq.com>
2024-11-12 19:03:58 +0800

    引入WindiCSS、Element-Plus和Vue-Router

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- <button class="btn">按钮</button> -->
+    <router-view></router-view>
+</template>
+
+<script setup lang='ts'>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<style scoped>
+/* .btn {
+    @apply bg-yellow-500 text-light-100 px-2 py-2 rounded my-4 mx-4;
+} */
+</style>
---
+++ src/pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <el-result icon="error" title="错误的路由" sub-title="请检查地址栏输入">
+            <template #extra>
+                <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+            </template>
+        </el-result>
+    </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped>
+div {}
+</style>
